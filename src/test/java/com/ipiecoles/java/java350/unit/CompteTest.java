dccf65c7c5f94b6bf26abb3587a763ea2a107472
Remy-BESSOn <Remy-BESSOn@10.252.107.230>
2017-11-06 16:27:52 +0100

    AJOUT DE TESTES UNITAIRE CompteTest

--- src/test/java/com/ipiecoles/java/java350/unit/CompteTest.java
+++ src/test/java/com/ipiecoles/java/java350/unit/CompteTest.java
@@ -24,4 +24,51 @@ public class CompteTest {
         Assertions.assertThat(c.getNumeroCompte()).isEqualTo("123");
         Assertions.assertThat(c.getValeurCourante()).isEqualTo(500.0);
     }
+    
+    @Test
+    public void test_Compte_Epargne(){
+        //Given
+        String input = "J" + TestUtils.LINE_SEPARATOR + "123" + TestUtils.LINE_SEPARATOR + "500" + TestUtils.LINE_SEPARATOR;
+        ByteArrayInputStream inContent = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inContent);
+
+        //When
+        Compte c = new Compte();
+
+        //Then
+        Assertions.assertThat(c.getTypeCompte()).isEqualTo("Joint");
+        Assertions.assertThat(c.getNumeroCompte()).isEqualTo("123");
+        Assertions.assertThat(c.getValeurCourante()).isEqualTo(500.0);
+    }
+    
+    @Test
+    //Should prevent the false epargne type and force the user to choose a valid count type
+    public void test_Compte_Epargne_With_False_String(){
+        //Given
+        String input = "F" + TestUtils.LINE_SEPARATOR + "123" + TestUtils.LINE_SEPARATOR + "500" + TestUtils.LINE_SEPARATOR;
+        ByteArrayInputStream inContent = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inContent);
+
+        //When
+        Compte c = new Compte("NotEparnge");
+
+        //Then
+        Assertions.assertThat(c.getTypeCompte()).isNotEqualTo("NotEpargne");
+        Assertions.assertThat(c.getTypeCompte()).isEqualTo("Courant");
+    }
+ 
+    @Test
+    public void Should_Not_Let_The_Initial_Value_To_A_Negative_Value(){
+        //Given
+        String input = "C" + TestUtils.LINE_SEPARATOR + "123" + TestUtils.LINE_SEPARATOR + "500" + TestUtils.LINE_SEPARATOR;
+        ByteArrayInputStream inContent = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inContent);
+
+        //When
+        Compte c = new Compte();
+
+        //Then
+        Assertions.assertThat(c.getValeurCourante()).isNotEqualTo(-500.0);
+    }
+    
 }
